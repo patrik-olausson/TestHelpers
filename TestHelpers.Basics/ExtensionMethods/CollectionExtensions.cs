df7bad14d92643c80c04cbb36763c6619964fb3c
Patrik Olausson <patrik.olausson@sigma.se>
2017-03-04 16:52:32 +0100

    Added some fundamental extension methods

---
+++ TestHelpers.Basics/ExtensionMethods/CollectionExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestHelpers.Basics
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Checks that both the list and the item are valid instances (not null) 
+        /// before adding the item to the list.
+        /// </summary>
+        public static void AddIfNotNull<T>(this List<T> items, T item)
+            where T : class
+        {
+            if (items == null) return;
+            if (item == null) return;
+
+            items.Add(item);
+        }
+
+        /// <summary>
+        /// "Safe" version of ForEach that checks if the collection is null before trying 
+        /// to iterate it.
+        /// </summary>
+        public static void ForEach<T>(this IReadOnlyCollection<T> items, Action<T> action)
+        {
+            if (items == null) return;
+
+            foreach (var item in items)
+            {
+                action(item);
+            }
+        }
+
+        /// <summary>
+        /// Wraps an object in an array and returns it as a IReadOnlyList
+        /// </summary>
+        public static IReadOnlyList<T> AsReadOnlyList<T>(this T @object)
+        {
+            return new[] { @object };
+        }
+
+        /// <summary>
+        /// Wraps an object in an array and returns it as a IReadOnlyCollection
+        /// </summary>
+        public static IReadOnlyCollection<T> AsReadOnlyCollection<T>(this T @object)
+        {
+            return new[] { @object };
+        }
+    }
+}
---
+++ TestHelpers.Basics/ExtensionMethods/JsonSerializationExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+
+namespace TestHelpers.Basics
+{
+    public static class JsonSerializationExtensions
+    {
+        /// <summary>
+        /// Takes any object and tries to serialize it to a JSON string using JsonConvert
+        /// and some default settings. Very useful when using ApprovalTests.
+        /// </summary>
+        /// <param name="objectToSerialize">The object you want to serialize.</param>
+        /// <param name="indented">Specify if you want the JSON to be indented or not. 
+        /// Since this is targeted for testing the default value is True.</param>
+        /// <returns>A JSON string representation of the provided object.</returns>
+        public static string ToJsonString(this object objectToSerialize, bool indented = true)
+        {
+            return JsonConvert.SerializeObject(
+                objectToSerialize,
+                indented ? Formatting.Indented : Formatting.None);
+        }
+
+        /// <summary>
+        /// Takes a string containing JSON and tries to deserialize it into an object of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type of object you want to deserialize the JSON string into</typeparam>
+        /// <param name="stringContainingJson"></param>
+        /// <returns>The deserialized object.</returns>
+        public static T FromJsonString<T>(this string stringContainingJson)
+        {
+            return JsonConvert.DeserializeObject<T>(stringContainingJson);
+        }
+    }
+}
